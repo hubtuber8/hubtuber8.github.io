5d76889eeeb806fb9a216dd49ab94ec329fb4cec
webb77788 <155199471+webb77788@users.noreply.github.com>
2024-02-14 08:23:48 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://hnetweb.nbzdwss78.tk'
+const BAREURL = 'bare.webpading.workers.dev'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
